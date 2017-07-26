791d76682f1fa96fc9b8916dfe3d230cda5a991f
Ercand <ercand83@yahoo.it>
2017-07-26 15:48:54 +0200

    delete example query

--- SQLQuery1.sql
+++
@@ -1,5 +0,0 @@
-﻿select * from Summoner t1, Summoner t2
-where (t1.ID != t2.ID and t1.IdSummoner = t2.IdSummoner)
-
-select * from PlayerLeague t1, PlayerLeague t2
-where (t1.ID != t2.ID and t1.SummonerId = t2.SummonerId)
